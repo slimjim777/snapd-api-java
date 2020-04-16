86671a50f627760cb4aa809e59032520dbb595a5
James Jesudason <james.jesudason@gmail.com>
2020-04-16 15:31:40 +0100

    Use later version of apache httpclient

--- src/main/java/com/canonical/App.java
+++ src/main/java/com/canonical/App.java
@@ -4,22 +4,31 @@ import com.canonical.snapdapi.SnapdAPI;
 import javafx.util.Pair;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 
 public class App
 {
     public static void main( String[] args ) {
+        // Initialize the API
+        SnapdAPI api = null;
         try {
-            // Initialize the API
-            SnapdAPI api = new SnapdAPI();
+            api = new SnapdAPI();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
 
-            // Get the system info from the API
-            Pair<Integer, String> response = api.getSystemInfo();
 
-            System.out.printf("HTTP Status: %d\n", response.getKey());
-            System.out.printf("Body:\n%s", response.getValue());
+        // Get the system info from the API
+        Pair<Integer, String> response = null;
+        try {
+            response = api.getSystemInfo();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.printf("HTTP Status: %d\n", response.getKey());
+        System.out.printf("Body:\n%s", response.getValue());
     }
+
 }
--- src/main/java/com/canonical/snapdapi/SnapdAPI.java
+++ src/main/java/com/canonical/snapdapi/SnapdAPI.java
@@ -1,32 +1,49 @@
 package com.canonical.snapdapi;
 
 
-import com.canonical.unixsocket.UnixProtocolSocketFactory;
 import javafx.util.Pair;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.protocol.Protocol;
+import com.canonical.socketfactory.UnixConnectionSocketFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
 
-import java.io.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 public class SnapdAPI {
-    final String snapdSocket = "/run/snapd.socket";
-    private HttpClient client;
+    private final CloseableHttpClient httpclient;
 
-    public SnapdAPI() throws IOException {
-        // Initialize the HTTP client to handle the Snapd socket connection
-        Protocol unix = new Protocol("unix", new UnixProtocolSocketFactory(snapdSocket), 80);
-        client = new HttpClient();
-        client.getHostConfiguration().setHost("localhost", 80, unix);
+
+    public SnapdAPI() throws URISyntaxException {
+        Registry<ConnectionSocketFactory> socketFactoryRegistry;
+
+        // Register a socket factory to handle the unix:// protocol
+        socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", new UnixConnectionSocketFactory(new URI("unix:///run/snapd.socket")))
+                .build();
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+
+        httpclient = HttpClients.custom()
+                .setConnectionManager(connManager)
+                .build();
     }
 
     public Pair<Integer, String> getSystemInfo() throws IOException {
-        GetMethod httpGet = new GetMethod("/v2/system-info");
-        client.executeMethod(httpGet);
+        HttpGet httpget = new HttpGet("http://localhost/v2/system-info");
+        CloseableHttpResponse response;
 
-        Pair<Integer, String> resp = new Pair<>(httpGet.getStatusCode(), httpGet.getResponseBodyAsString());
-        httpGet.releaseConnection();
+        response = httpclient.execute(httpget);
+        HttpEntity body = response.getEntity();
+        Pair<Integer, String> resp = new Pair<>(response.getStatusLine().getStatusCode(), EntityUtils.toString(body));
         return resp;
     }
 }
